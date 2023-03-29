e279008e83c2e4642db8ac660f44ed58819207e2
Adam S <adam.said.1901@gmail.com>
2023-03-29 14:03:34 +0200

    [AS] Added link to firebase and user creation

--- app/src/main/java/com/example/interim/UserRegistrationActivity.java
+++ app/src/main/java/com/example/interim/UserRegistrationActivity.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.example.interim.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
