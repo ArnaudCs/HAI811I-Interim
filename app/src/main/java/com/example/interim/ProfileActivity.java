5abc3aa48a4010393193e07462cb1ec7d5d2629d
Lanmark1 <jeremie.bentolila@gmail.com>
2023-03-29 18:47:24 +0200

    [JB] Ajout page profil

---
+++ app/src/main/java/com/example/interim/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.interim;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ProfileActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+    }
+}
