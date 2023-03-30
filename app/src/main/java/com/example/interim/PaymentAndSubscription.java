e72bfb0e43943a681ca6360343b36ad9a388b3e7
ArnaudCs <arnaud.cossu@gmail.com>
2023-03-30 13:31:36 +0200

    [AC] Ajout de la page choose plan et de ses traductions

---
+++ app/src/main/java/com/example/interim/PaymentAndSubscription.java
@@ -0,0 +1,14 @@
+package com.example.interim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentAndSubscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_and_subscription);
+    }
+}
