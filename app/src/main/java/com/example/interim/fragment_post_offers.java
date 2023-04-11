29f12e274e30f5097f0917da7f5b7f91bc3c04d6
ArnaudCs <arnaud.cossu@gmail.com>
2023-04-11 10:01:03 +0200

    [AC] Changement de la couleur des inputs (accent) et ajout de datePicker

--- app/src/main/java/com/example/interim/fragment_post_offers.java
+++ app/src/main/java/com/example/interim/fragment_post_offers.java
@@ -1,6 +1,8 @@
 package com.example.interim;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -12,12 +14,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.storage.FileDownloadTask;
@@ -26,6 +31,8 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 
 public class fragment_post_offers extends Fragment {
     public fragment_post_offers() {
@@ -42,7 +49,54 @@ public class fragment_post_offers extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        TextInputLayout layoutDateStartOffer = view.findViewById(R.id.layoutDateStartOffer);
+        TextInputEditText textDateStartOffer = view.findViewById(R.id.textDateStartOffer);
+        TextInputEditText textDateEndOffer = view.findViewById(R.id.textDateEndOffer);
+        DatePickerDialog.OnDateSetListener setListener;
 
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DATE);
 
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month+1;
+                String date = day+"/"+month+"/"+year;
+            }
+        };
+
+        textDateStartOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        textDateStartOffer.setText(date);
+                    }
+                }, year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+        textDateEndOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        textDateEndOffer.setText(date);
+                    }
+                }, year,month,day);
+                datePickerDialog.show();
+            }
+        });
     }
 }
