970c8f70507aedf05c0a0215f72b01253a7d7fee
ArnaudCs <arnaud.cossu@gmail.com>
2023-04-14 16:11:19 +0200

    [AC] Ajout du script navbar offers, et modification de la page offers

--- app/src/main/java/com/example/interim/fragment_post_offers.java
+++ app/src/main/java/com/example/interim/fragment_post_offers.java
@@ -13,14 +13,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
@@ -31,8 +35,10 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class fragment_post_offers extends Fragment {
     public fragment_post_offers() {
@@ -49,15 +55,65 @@ public class fragment_post_offers extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TextInputLayout layoutDateStartOffer = view.findViewById(R.id.layoutDateStartOffer);
-        TextInputEditText textDateStartOffer = view.findViewById(R.id.textDateStartOffer);
-        TextInputEditText textDateEndOffer = view.findViewById(R.id.textDateEndOffer);
+        TextInputLayout layoutDateStartOffer = view.findViewById(R.id.dateOfferStartLayout);
+        TextInputLayout layoutDateEndOffer = view.findViewById(R.id.dateOfferEndLayout);
+
+        TextInputEditText textDateStartOffer = view.findViewById(R.id.textOfferStartDate);
+        TextInputEditText textDateEndOffer = view.findViewById(R.id.textOfferEndDate);
+
+        BottomNavigationView bottomNav = getActivity().findViewById(R.id.navbar);
         DatePickerDialog.OnDateSetListener setListener;
+        Spinner categoryOfferChoice = view.findViewById(R.id.categoryOfferChoice);
+        ScrollView formScrollContainer = view.findViewById(R.id.formScrollContainer);
+
+
+        List<String> spinnerArray =  new ArrayList<String>();
+        spinnerArray.add(getResources().getString(R.string.chooseCat));
+        spinnerArray.add("Chantier et BTP");
+        spinnerArray.add("Nettoyage");
+        spinnerArray.add("Informatique");
+        spinnerArray.add("Nettoyage");
+        spinnerArray.add("Manutention");
+        spinnerArray.add("Automobile");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoryOfferChoice.setAdapter(adapter);
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DATE);
+
+        formScrollContainer.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            private int scrollThreshold = 10;
+            private int scrolledDistance = 0;
+            private boolean isScrollingDown = false;
+
+            @Override
+            public void onScrollChange(View view, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                int currentScrollPosition = scrollY;
+
+                if (scrolledDistance > scrollThreshold && isScrollingDown ) {
+                    // Hide BottomNavigationView
+                    bottomNav.animate().translationY(bottomNav.getHeight() + 100).setDuration(200);
+                    scrolledDistance = 0;
+                    isScrollingDown = false;
+                    System.out.println("Scrolling down");
+                } else if (scrolledDistance < -scrollThreshold && !isScrollingDown) {
+                    // Show BottomNavigationView
+                    bottomNav.animate().translationY(0).setDuration(200);
+                    scrolledDistance = 0;
+                    isScrollingDown = true;
+                    System.out.println("Scrolling up");
+                }
+
+                if ((isScrollingDown && scrollY > oldScrollY) || (!isScrollingDown && scrollY < oldScrollY)) {
+                    scrolledDistance += (scrollY - oldScrollY);
+                }
+            }
+        });
 
         setListener = new DatePickerDialog.OnDateSetListener() {
             @Override
