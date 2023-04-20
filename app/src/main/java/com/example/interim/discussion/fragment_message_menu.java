1f8b75d1eeb668147d4c37ef9444452211ca7303
Adam S <adam@adam-net.fr>
2023-04-20 14:17:17 +0200

    [AS] Fixed retrieve conv for Pros

--- app/src/main/java/com/example/interim/discussion/fragment_message_menu.java
+++ app/src/main/java/com/example/interim/discussion/fragment_message_menu.java
@@ -1,5 +1,6 @@
 package com.example.interim.discussion;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.interim.R;
+import com.example.interim.authentication.MainActivity;
 import com.example.interim.models.Conversation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,6 +35,7 @@ public class fragment_message_menu extends Fragment {
 
     private conversation_ViewAdapter mAdapter;
     Button deleteMessages, cancelDelete;
+    String type;
 
     public fragment_message_menu() {
         // Required empty public constructor
@@ -42,6 +45,15 @@ public class fragment_message_menu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() == null){
+            Intent mainActivity = new Intent(getActivity(), MainActivity.class);
+            startActivity(mainActivity);
+            this.getActivity().finish();
+            return null;
+        }
         return inflater.inflate(R.layout.fragment_message_menu, container, false);
     }
 
@@ -57,98 +69,116 @@ public class fragment_message_menu extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         mAdapter = new conversation_ViewAdapter(getContext(), conversations);
         recyclerView.setAdapter(mAdapter);
+        type = "Pros";
+        db.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+           @Override
+           public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+               if (task.isSuccessful()) {
+                   DocumentSnapshot document = task.getResult();
+                   if (document.exists()) {
+                       type = "Users";
+                   } else {
+                       type = "Pros";
+                   }
 
+                   db.collection("Conversations")
+                           .whereArrayContains("participants", db.collection(type).document(userId))
+                           .get()
+                           .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                               @Override
+                               public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                   final String[] participantName = new String[1];
+                                   if (task.isSuccessful()) {
+                                       System.out.println("ID " + userId.toString());
 
-        db.collection("Conversations")
-                .whereArrayContains("participants", db.collection("Users").document(userId))
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        final String[] participantName = new String[1];
-                        if (task.isSuccessful()) {
+                                       for (QueryDocumentSnapshot document : task.getResult()) {
+                                           System.out.println(document.toString());
 
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                // Extract the Conversation data from the Firestore document
-                                String otherParticipant = "";
-                                String lastMessage = "";
-                                Conversation conversation = new Conversation(document.getId(), otherParticipant, false, lastMessage);
-                                List<DocumentReference> participantsRefs = (List<DocumentReference>) document.get("participants");
+                                           // Extract the Conversation data from the Firestore document
+                                           String otherParticipant = "";
+                                           String lastMessage = "";
+                                           Conversation conversation = new Conversation(document.getId(), otherParticipant, false, lastMessage);
+                                           List<DocumentReference> participantsRefs = (List<DocumentReference>) document.get("participants");
 
-                                assert participantsRefs != null;
-                                for (DocumentReference participantRef : participantsRefs) {
-                                    String participantId = participantRef.getId();
-                                    if (!participantId.equals(userId)) {
-                                        // Get the other participant's name from the Users or Pros collection
-                                        String participantType = participantRef.getParent().getId();
+                                           assert participantsRefs != null;
+                                           for (DocumentReference participantRef : participantsRefs) {
+                                               String participantId = participantRef.getId();
+                                               if (!participantId.equals(userId)) {
+                                                   // Get the other participant's name from the Users or Pros collection
+                                                   String participantType = participantRef.getParent().getId();
 
-                                        if (participantType.equals("Users")) {
-                                            DocumentReference participantDocRef = db.collection("Users").document(participantId);
-                                            participantDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                    if (task.isSuccessful()) {
-                                                        DocumentSnapshot participantDoc = task.getResult();
-                                                        participantName[0] = participantDoc.getString("firstName");
-                                                        conversation.setContact(participantName[0]);
+                                                   if (participantType.equals("Users")) {
+                                                       DocumentReference participantDocRef = db.collection("Users").document(participantId);
+                                                       participantDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                           @Override
+                                                           public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                               if (task.isSuccessful()) {
+                                                                   DocumentSnapshot participantDoc = task.getResult();
+                                                                   participantName[0] = participantDoc.getString("firstName");
+                                                                   conversation.setContact(participantName[0]);
 
-                                                        // Set the RecyclerView adapter with the updated list of Conversations
-                                                        mAdapter.notifyDataSetChanged();
-                                                        recyclerView.setAdapter(mAdapter);
+                                                                   // Set the RecyclerView adapter with the updated list of Conversations
+                                                                   mAdapter.notifyDataSetChanged();
+                                                                   recyclerView.setAdapter(mAdapter);
 
-                                                    }
-                                                }
-                                            });
-                                        } else if (participantType.equals("Pros")) {
-                                            DocumentReference participantDocRef = db.collection("Pros").document(participantId);
-                                            participantDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                    if (task.isSuccessful()) {
-                                                        DocumentSnapshot participantDoc = task.getResult();
-                                                        participantName[0] = participantDoc.getString("name");
-                                                        conversation.setContact(participantName[0]);
+                                                               }
+                                                           }
+                                                       });
+                                                   } else if (participantType.equals("Pros")) {
+                                                       DocumentReference participantDocRef = db.collection("Pros").document(participantId);
+                                                       participantDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                           @Override
+                                                           public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                               if (task.isSuccessful()) {
+                                                                   DocumentSnapshot participantDoc = task.getResult();
+                                                                   participantName[0] = participantDoc.getString("name");
+                                                                   conversation.setContact(participantName[0]);
 
-                                                        // Set the RecyclerView adapter with the updated list of Conversations
-                                                        mAdapter.notifyDataSetChanged();
-                                                        recyclerView.setAdapter(mAdapter);
+                                                                   // Set the RecyclerView adapter with the updated list of Conversations
+                                                                   mAdapter.notifyDataSetChanged();
+                                                                   recyclerView.setAdapter(mAdapter);
 
-                                                    }
-                                                }
-                                            });
-                                        }
+                                                               }
+                                                           }
+                                                       });
+                                                   }
 
 //                                        conversation.setContact(participantName[0]);
-                                        break;
-                                    }
-                                }
+                                                   break;
+                                               }
+                                           }
 
-                                List<DocumentReference> unReadRef = (List<DocumentReference>) document.get("unRead");
-                                if (unReadRef != null) {
-                                    for (DocumentReference userUnReadRef : unReadRef) {
-                                        String userUnReadId = userUnReadRef.getId();
-                                        if (userUnReadId.equals(userId)) {
-                                            conversation.setUnread(true);
-                                            break;
-                                        }
-                                    }
-                                }
+                                           List<DocumentReference> unReadRef = (List<DocumentReference>) document.get("unRead");
+                                           if (unReadRef != null) {
+                                               for (DocumentReference userUnReadRef : unReadRef) {
+                                                   String userUnReadId = userUnReadRef.getId();
+                                                   if (userUnReadId.equals(userId)) {
+                                                       conversation.setUnread(true);
+                                                       break;
+                                                   }
+                                               }
+                                           }
 
-                                if (document.contains("lastMessage")) {
-                                    lastMessage = document.getString("lastMessage");
-                                    conversation.setLastMsg(lastMessage);
-                                }
-                                conversations.add(conversation);
-                            }
-                            // Set the RecyclerView adapter with the list of Conversations
-                            RecyclerView recyclerView = view.findViewById(R.id.conversationsRecycler);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                            recyclerView.setAdapter(new conversation_ViewAdapter(getContext(), conversations));
-                        } else {
-                            Log.e("TAG", "No conversations to retrieve");
-                        }
-                    }
-                });
+                                           if (document.contains("lastMessage")) {
+                                               lastMessage = document.getString("lastMessage");
+                                               conversation.setLastMsg(lastMessage);
+                                           }
+                                           conversations.add(conversation);
+                                       }
+                                       // Set the RecyclerView adapter with the list of Conversations
+                                       RecyclerView recyclerView = view.findViewById(R.id.conversationsRecycler);
+                                       recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                                       recyclerView.setAdapter(new conversation_ViewAdapter(getContext(), conversations));
+                                   } else {
+                                       Log.e("TAG", "No conversations to retrieve");
+                                   }
+                               }
+                           });
+               }
+           }
+       });
+
+
 
 
         deleteMessages.setOnClickListener(new View.OnClickListener() {
