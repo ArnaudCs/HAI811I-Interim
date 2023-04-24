13e08abd1b555ade872a1e623fe3c029a5a5c680
Adam S <adam@adam-net.fr>
2023-04-24 15:18:05 +0200

    [AS] Added My offers for PROS

--- app/src/main/java/com/example/interim/fragment_my_offers_company.java
+++ app/src/main/java/com/example/interim/fragment_my_offers_company.java
@@ -2,21 +2,101 @@ package com.example.interim;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.interim.models.Offer;
+import com.example.interim.offers.searchCard_ViewAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class fragment_my_offers_company extends Fragment {
     public fragment_my_offers_company() {
         // Required empty public constructor
     }
+    RecyclerView recyclerView ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_my_offers_company, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        RecyclerView recyclerView = view.findViewById(R.id.cardContainer);
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        BottomNavigationView bottomNav = getActivity().findViewById(R.id.navbar);
+        recyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            private int scrollThreshold = 10;
+            private int scrolledDistance = 0;
+            private boolean isScrollingDown = false;
+
+            @Override
+            public void onScrollChange(View view, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                int currentScrollPosition = scrollY;
+
+                if (scrolledDistance > scrollThreshold && isScrollingDown) {
+                    // Hide BottomNavigationView
+                    bottomNav.animate().translationY(bottomNav.getHeight() + 100).setDuration(200);
+                    scrolledDistance = 0;
+                    isScrollingDown = false;
+                } else if (scrolledDistance < -scrollThreshold && !isScrollingDown) {
+                    // Show BottomNavigationView
+                    bottomNav.animate().translationY(0).setDuration(200);
+                    scrolledDistance = 0;
+                    isScrollingDown = true;
+                }
+
+                if ((isScrollingDown && scrollY > oldScrollY) || (!isScrollingDown && scrollY < oldScrollY)) {
+                    scrolledDistance += (scrollY - oldScrollY);
+                }
+            }
+        });
+
+
+        db.collection("Offers")
+                .whereEqualTo("recruiter", userId)
+                .orderBy("postDate", Query.Direction.DESCENDING)
+                .limit(50)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot querySnapshot) {
+                        List<Offer> offers = new ArrayList<>();
+                        for (QueryDocumentSnapshot documentSnapshot : querySnapshot) {
+                            Offer offer = documentSnapshot.toObject(Offer.class);
+                            offer.setId(documentSnapshot.getId());
+                            offers.add(offer);
+                        }
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                        recyclerView.setAdapter(new searchCard_ViewAdapter(getContext(), offers));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 }
