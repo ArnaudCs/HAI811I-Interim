e841e57c6e67ae74303138cda857e395283b29cd
Gaiko19 <adam.said.1901@gmail.com>
2023-04-26 22:59:19 +0100

    [AS] Work my applications page

--- app/src/main/java/com/example/interim/offers/fragment_my_applications.java
+++ app/src/main/java/com/example/interim/offers/fragment_my_applications.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,14 +16,27 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.interim.R;
+import com.example.interim.models.Offer;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 
+import java.util.ArrayList;
+
 public class fragment_my_applications extends Fragment {
+    String userId;
+    RecyclerView recyclerView;
+    ArrayList<Offer> offers;
 
     public fragment_my_applications() {
         // Required empty public constructor
@@ -37,6 +52,56 @@ public class fragment_my_applications extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button backButtonApplications = view.findViewById(R.id.backButtonApplications);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null) {
+            userId = mAuth.getCurrentUser().getUid();
+        }
+
+        if(userId != null) {
+            db.collection("Applications")
+                    .whereEqualTo("applicantId", userId)
+                    .get()
+                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                        @Override
+                        public void onSuccess(QuerySnapshot querySnapshot) {
+                            ArrayList<String> offerIds = new ArrayList<>();
+                            for (QueryDocumentSnapshot documentSnapshot : querySnapshot) {
+                                offerIds.add(documentSnapshot.getString("offerId"));
+                            }
+                            db.collection("Offers")
+                                    .whereIn(FieldPath.documentId(), offerIds)
+                                    .get()
+                                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onSuccess(QuerySnapshot querySnapshot) {
+                                            offers = new ArrayList<>();
+                                            for (QueryDocumentSnapshot documentSnapshot : querySnapshot) {
+                                                Offer offer = documentSnapshot.toObject(Offer.class);
+                                                offer.setId(documentSnapshot.getId());
+                                                offers.add(offer);
+                                            }
+                                            recyclerView = view.findViewById(R.id.favoriteContainer);
+                                            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                                            recyclerView.setAdapter(new searchCard_ViewAdapter(getContext(), offers));
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Handle the error
+                                        }
+                                    });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Handle the error
+                        }
+                    });
+        }
+
 
         backButtonApplications.setOnClickListener(new View.OnClickListener() {
             @Override
