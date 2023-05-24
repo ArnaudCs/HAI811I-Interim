e59baf23a010f7a6536fca59fc3630cafcfce0e7
Lanmark1 <jeremie.bentolila@gmail.com>
2023-05-24 12:44:55 +0200

    [JB] Mode nuit pour les annonces favorites

--- app/src/main/java/com/example/interim/offers/FavoritesCompanyActivity.java
+++ app/src/main/java/com/example/interim/offers/FavoritesCompanyActivity.java
@@ -3,6 +3,7 @@ package com.example.interim.offers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.example.interim.R;
@@ -14,6 +15,12 @@ public class FavoritesCompanyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode &
+                Configuration.UI_MODE_NIGHT_MASK;
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES)
+            setTheme(R.style.ThemeDark_Interim);
+        else
+            setTheme(R.style.Theme_Interim);
         setContentView(R.layout.activity_favorites_company);
 
         Intent intent = getIntent();
