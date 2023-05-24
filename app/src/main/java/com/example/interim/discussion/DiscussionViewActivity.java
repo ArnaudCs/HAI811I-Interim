61515bd29b699cc503e29a01b96f7c1221f69e93
Lanmark1 <jeremie.bentolila@gmail.com>
2023-05-24 12:17:40 +0200

    [JB] Mode nuit pour discussions

--- app/src/main/java/com/example/interim/discussion/DiscussionViewActivity.java
+++ app/src/main/java/com/example/interim/discussion/DiscussionViewActivity.java
@@ -2,6 +2,7 @@ package com.example.interim.discussion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.example.interim.Utils.DataHolder;
@@ -13,6 +14,12 @@ public class DiscussionViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Retrieve conversation ID extra from the intent
+        int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode &
+                Configuration.UI_MODE_NIGHT_MASK;
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES)
+            setTheme(R.style.ThemeDark_Interim);
+        else
+            setTheme(R.style.Theme_Interim);
         String conversationId = getIntent().getStringExtra("conversationId");
         DataHolder.getInstance().setConversationId(conversationId);
         setContentView(R.layout.activity_discussion_view);
