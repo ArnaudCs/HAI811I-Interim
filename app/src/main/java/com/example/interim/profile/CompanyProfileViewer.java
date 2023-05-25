4de0718be1a6ec647684f7cbd8f887088485ba91
Lanmark1 <jeremie.bentolila@gmail.com>
2023-05-25 16:03:49 +0200

    [JB] Ajout mode nuit page mission

--- app/src/main/java/com/example/interim/profile/CompanyProfileViewer.java
+++ app/src/main/java/com/example/interim/profile/CompanyProfileViewer.java
@@ -3,6 +3,7 @@ package com.example.interim.profile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.example.interim.R;
@@ -14,6 +15,12 @@ public class CompanyProfileViewer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode &
+                Configuration.UI_MODE_NIGHT_MASK;
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES)
+            setTheme(R.style.ThemeDark_Interim);
+        else
+            setTheme(R.style.Theme_Interim);
         setContentView(R.layout.activity_company_profile_viewer);
 
         Intent intent = getIntent();
